21e61d36c725afae9fb7cb5cacd08b2cb00de5ec
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-10 09:45:49 -0600

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,12 @@
 <template>
-  <header>
-    <div>
+  <header class="text-center bg-blue-500 text-white p-4 mb-10">
+    <div class="txt-3xl md:text-5xl font-bold">
+      <i class="fas fa-viruses"></i>
       Covid-19 Tracker
     </div>
+    <a class="text-blue-900" target="_blank" href="https://covid19api.com"
+      >covid19api.com</a
+    >
   </header>
 </template>
 
