f67c9e54d9de1fce3feb74bfa596c4400b1e7afc
Vadym Hulchenko <vadymhulchenko@gmail.com>
2024-10-23 02:36:37 -0300

    rework loading component, add skeleton placeholders

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,3 @@
-//TODO test with no data
-
 <script setup>
 import { ref, onMounted } from "vue";
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -9,10 +7,8 @@ import DataBoxes from "@/components/DataBoxes.vue";
 import CountrySelect from "@/components/CountrySelect.vue";
 
 const loading = ref(true);
-const loadingImage = new URL("../assets/loading.gif", import.meta.url).href;
-
+const currCountry = ref("Canada");
 const stats = ref({
-  country: "",
   confirmed: 0,
   critical: 0,
   deaths: 0,
@@ -30,7 +26,6 @@ const fetchCovidData = async (country = "Canada") => {
       },
     });
     const data = await res.json();
-    console.log(`DATA: `, data);
 
     if (Array.isArray(data)) {
       return data[0];
@@ -42,21 +37,13 @@ const fetchCovidData = async (country = "Canada") => {
 };
 
 const countryChangeHandler = async (country) => {
-  console.log(`SELECTED COUNTRY: `, country);
   const data = await fetchCovidData(country);
   stats.value = data;
+  currCountry.value = country;
   loading.value = false;
 };
 
-const clearCountryData = async (e) => {
-  // loading.value = true;
-  // const data = await fetchCovidData();
-  // stats.value = "Canada";
-  // loading.value = false;
-};
-
 // Lifecycle hook on init load
-
 onMounted(async () => {
   const data = await fetchCovidData();
   stats.value = data;
@@ -65,22 +52,58 @@ onMounted(async () => {
 </script>
 
 <template>
-  <main v-if="!loading">
+  <main v-if="!loading" class="mt-44">
     <DataTitle :stats="stats" />
     <DataBoxes :stats="stats" />
-    <CountrySelect @changeCountry="countryChangeHandler" :loading="loading" />
-
-    <button
-      @click="clearCountryData"
-      v-if="stats?.Country"
-      class="float-right bg-blue-500 text-white rounded p-5 w-3/12 mt-10 focus:outline-none hover:bg-blue-400 border-red-500"
-    >
-      Reset
-    </button>
+    <CountrySelect @changeCountry="countryChangeHandler" @updateLoading="loading = $event" v-model="currCountry" />
   </main>
 
-  <main class="flex flex-col align-center justify-center text-center" v-else>
-    <div class="text-red-400 text-2xl mt-10 mb-6">Fetching Data</div>
-    <img :src="loadingImage" class="w-14 m-auto" alt="loading" />
-  </main>
+  <div class="mt-44" v-else>
+    <div class="animate-pulse text-center border-red-400">
+      <h2 class="text-3xl font-bold">Loading...</h2>
+      <div class="text-2xl mt-4 mb-10 text-white">.</div>
+    </div>
+
+    <div class="grid md:grid-cols-2 gap-4">
+      <div class="shadow-md bg-indigo-50 p-10 h-44 text-center rounded">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-12 py-1">
+            <div class="h-3 bg-indigo-500 rounded"></div>
+            <div class="h-2 bg-slate-500 rounded"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="shadow-md bg-indigo-50 p-10 h-44 text-center rounded">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-12 py-1">
+            <div class="h-3 bg-orange-500 rounded"></div>
+            <div class="h-2 bg-slate-500 rounded"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="shadow-md bg-indigo-50 p-10 h-44 text-center rounded">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-12 py-1">
+            <div class="h-3 bg-red-400 rounded"></div>
+            <div class="h-2 bg-slate-500 rounded"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="shadow-md bg-indigo-50 p-10 h-44 text-center rounded">
+        <div class="animate-pulse flex space-x-4">
+          <div class="flex-1 space-y-12 py-1">
+            <div class="h-3 bg-green-500 rounded"></div>
+            <div class="h-2 bg-slate-500 rounded"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <select class="form-select block w-full border p-3 rounded mt-10 pointer-events-none">
+      <option>{{ currCountry }}</option>
+    </select>
+  </div>
 </template>
